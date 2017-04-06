618c3d459072c077c1a29e4fcf1496c1ba86bd9c
austin <austinogilvie@gmail.com>
2017-04-06 16:16:51 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Generate all valid combinations of your gmail address with dots in between each character in your username.
+
+```
+python gmailcombos.py "example@gmail.com"
+```
---
+++ gmailcombos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+"""gmailcombos.py
+"""
+import sys
+import os
+
+def get_combos(gmail_addr, sep="."):
+    combos = []
+    gmail_addr = "".join(ch for ch in gmail_addr if ch != ".")
+    username = gmail_addr.split("@")[0]
+    letters = list(username)
+    for i, letter in enumerate(letters):
+        if i == 0 or i == len(letters):
+            continue
+        perm = "".join(letters[:i])+"."+"".join(letters[i:])
+        combos.append(perm+"@gmail.com")
+    return combos
+
+def main():
+    gmail = sys.argv[1]
+    for perm in get_combos(gmail):
+        print perm
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
+
